37bc003cb6ee614c86dedd8c73aaf00ef826e7b1
matthewrgoff <43281480+matthewrgoff@users.noreply.github.com>
2018-10-08 22:04:00 -0500

    Add files via upload

---
+++ dice-roller.js
@@ -0,0 +1,9 @@
+// Matt Goff
+
+function RollSixSidedDice() {
+	 document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+	 document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+	 document.getElementById("dice3").value = Math.floor(Math.random()*6) + 1;
+	 document.getElementById("dice4").value = Math.floor(Math.random()*6) + 1;
+	 document.getElementById("dice5").value = Math.floor(Math.random()*6) + 1;
+}
