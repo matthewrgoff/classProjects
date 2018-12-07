4179a80378e4707bf3c1536f5e898b665f0bd2bd
matthewrgoff <matthewrgoff@lewisu.edu>
2018-12-07 12:38:58 -0600

    fixed php

--- saveCity.php
+++ saveCity.php
@@ -2,8 +2,13 @@
     // Matt Goff
 	
 	$cityPHP = $_GET["newCity"];
-	$myfile = fopen("cityJSON.json", "w") or die("Unable to open file to write!");
-	fwrite($myfile, $cityPHP);
-    fclose($myfile);
+	$cityName = array('city'=>$cityPHP);
+	$toWrite = json_encode($cityName);
+	file_put_contents('cityJSON.json', $toWrite);
+	//$myfile = fopen("cityJSON.json", "w") or die("Unable to open file to write!");
+	//$myfile = file_get_contents("cityJSON.json");
+	//fwrite($myfile, $cityPHP);
+    //fclose($myfile);
     echo "City Saved";
+	
 ?>
